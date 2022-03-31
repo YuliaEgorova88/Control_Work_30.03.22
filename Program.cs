ee2fc36865ab90fc4f579ccede35209012a90f0c
Yulia Egorova <youlkin@mail.ru>
2022-03-31 16:26:51 +0300

    string massive is created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] someWords = { "hello", "world", "2", "ups", "cat", "me", "eagle" };
