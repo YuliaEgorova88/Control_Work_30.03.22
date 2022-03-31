22920516b2d3a8e6539db89d7841b2a848b87678
Yulia Egorova <youlkin@mail.ru>
2022-03-31 19:23:17 +0300

    task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿
-string[] someWords = { "hello", "world", "2", "ups", "cat", "me", "eagle" };
+string[] someWords = new string[7] { "hello", "world", "2", "ups", "cat", "me", "eagle" };
+string[] newWords = new string[someWords.Length];
+
+int count = 0;
+
+void CorrectMassive(string[] arr1, string[] arr2)
+{
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+CorrectMassive(someWords, newWords);
+PrintArray(newWords);
